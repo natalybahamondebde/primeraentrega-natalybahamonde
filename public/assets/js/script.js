819137e6e560c0adb06afcc998edf3f6e9e6fb61
natalybahamondebde <natalymaria.bahamonde@alumnos.ulagos.cl>
2025-09-23 21:56:50 -0300

    Update script.js

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,5 +1,5 @@
 function changeImage(shirtId, img1, img2) {
     const shirt = document.getElementById(shirtId);
-    shirt.style.setProperty('--img1', `url(${img1})`);
-    shirt.style.setProperty('--img2', `url(${img2})`);
+    shirt.style.setProperty('--img1', `url(${img1}')`);
+    shirt.style.setProperty('--img2', `url(${img2}')`);
 }
